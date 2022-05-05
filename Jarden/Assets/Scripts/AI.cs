64efce43f81a9c1021a6167963feb71eb9983a13
Ceruin <13532471+Ceruin@users.noreply.github.com>
2022-05-05 00:30:18 -0400

    Commit

---
+++ Jarden/Assets/Scripts/AI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    // todo: ai (ml agents?)
+    // todo: ai physics (unity?)
+    // todo: ai movement
+    // todo: ai consumption
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
