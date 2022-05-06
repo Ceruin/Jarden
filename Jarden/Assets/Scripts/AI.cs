d8b9ca83b7d8956f2173fb5863815ebd3cb15203
Ceruin <13532471+Ceruin@users.noreply.github.com>
2022-05-06 02:22:48 -0400

    .

--- Jarden/Assets/Scripts/AI.cs
+++ Jarden/Assets/Scripts/AI.cs
@@ -8,16 +8,19 @@ public class AI : MonoBehaviour
     // todo: ai physics (unity?)
     // todo: ai movement
     // todo: ai consumption
+    public float boostPower = 10f;
+    Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // some push force that sends it towards an object
+        body.AddForce(Vector3.up * boostPower, ForceMode.Impulse);
     }
 }
